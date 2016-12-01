5c38d8b8909dd6f15d27162e8a1092ed040e3a21
ianjorgensen <jorgensen.ian@gmail.com>
2016-12-01 09:30:45 +0100

    added sizing

---
+++ app.js
@@ -0,0 +1,18 @@
+$(function() {
+  $('.dashboard-brick-list > li').click(function() {
+    var newCard = $(this).clone();
+    $('.dashboard-brick-list').append(newCard);
+    refreshClass();
+  });
+  refreshClass();
+});
+
+var refreshClass = function() {
+  var cards = $('.dashboard-brick-list .demo-card');
+
+  if (cards.length > 2) {
+    cards.removeClass('mdl-card-big');
+  } else {
+    cards.addClass('mdl-card-big');
+  }
+}
